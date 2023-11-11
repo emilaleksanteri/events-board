5f1b2fabff8615970441db33abd85978a2561e1b
Emil Lystimaki <emil@circularway.com>
2023-11-11 21:21:02 +0000

    fix: 🐛 bad ideas

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -32,13 +32,8 @@ func (app *application) serve() error {
 
 		app.logger.Info("caught signal!", "signal", s.String())
 
-		ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), 120*time.Second)
 		defer cancel()
-
-		redisStatus := app.redis.Shutdown(ctx)
-		if redisStatus.Err() != nil {
-			shutdownError <- redisStatus.Err()
-		}
 
 		err := srv.Shutdown(ctx)
 		if err != nil {
